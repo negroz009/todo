e4aff914d633e4097c9d261c047f78a5c3b6f8b2
myname <negroz009>
2022-08-24 14:30:34 +0100

    profilepage playing with DOM

---
+++ script/script.js
@@ -0,0 +1,24 @@
+let name="NEGROZ GEAR";
+const gender='Male';
+let age=36;
+let accountbalance=20000000
+let text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias perspiciatis <br> veritatis itaque expedita. Quas voluptas et voluptatum culpa reprehenderit <br> vitae rem natus laboriosam, quidem autem eveniet pariatur tempore beatae placeat."
+
+document.getElementById("profile-name").innerHTML=name;
+document.getElementById("text-name").innerHTML=text;
+
+
+
+
+
+
+
+
+
+
+
+/*Welcome to my world of coding where i have been priviledge to learn about software development fundamentals,<br>
+html, css and have written various codes on my own. Apart from the class lectures, the internet has been of good <br>
+help to my coding life especially the bootstrap frameworks and others. i am looking forward to learning about <br>
+Javascript and other necessary programming languages. Once again, welcome to my world of coding and feel free <br>
+to find out about me below:*/
