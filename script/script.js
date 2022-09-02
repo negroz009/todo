3e020c4debc2f5fd57f27ccb19bd142e2e923065
myname <negroz009>
2022-09-02 11:11:31 +0100

    profile page

--- script/script.js
+++ script/script.js
@@ -1,13 +1,11 @@
-let name="NEGROZ GEAR";
+let name="Negroz";
 const gender='Male';
 let age=36;
-let accountbalance=20000000
-let text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias perspiciatis <br> veritatis itaque expedita. Quas voluptas et voluptatum culpa reprehenderit <br> vitae rem natus laboriosam, quidem autem eveniet pariatur tempore beatae placeat."
-
-document.getElementById("profile-name").innerHTML=name;
-document.getElementById("text-name").innerHTML=text;
-
-
+let accountBalance=200;
+let goodBoy=true;
+let balanceOverAge=accountBalance/age;
+let answerVal=accountBalance%age;
+let a=2===2;
 
 
 
@@ -17,8 +15,12 @@ document.getElementById("text-name").innerHTML=text;
 
 
 
-/*Welcome to my world of coding where i have been priviledge to learn about software development fundamentals,<br>
-html, css and have written various codes on my own. Apart from the class lectures, the internet has been of good <br>
-help to my coding life especially the bootstrap frameworks and others. i am looking forward to learning about <br>
-Javascript and other necessary programming languages. Once again, welcome to my world of coding and feel free <br>
-to find out about me below:*/
+
+
+
+
+
+
+
+
+
